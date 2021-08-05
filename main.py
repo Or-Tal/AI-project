f9950b499b4a31df0ed2694e0a6aa929f462de1f
ort <or.tal@altair-semi.com>
2021-08-05 20:31:16 +0300

    updated main function

--- main.py
+++ main.py
@@ -49,6 +49,8 @@ def main_func(a):
 
 def parse_args():
     parser = argparse.ArgumentParser()
+
+    # global arguments
     parser.add_argument("--dset_path", required=False, help="path/to/dataset.npy\nif you wish to regenerate a dataset"
                                                             " and run using the generated don't pass anything.\n see "
                                                             "n, max_cost, max_rev, min_rev, save_path arguments")
@@ -56,12 +58,27 @@ def parse_args():
     parser.add_argument("--partition", required=False, help="partition function version - only for genetic", default=1)
     parser.add_argument("--city_selection", required=False, help="city selection function version - only for genetic", default=1)
 
+    # genetic alg related arguments
+    # -- generate dset case
     parser.add_argument("--n", default=100, help="num_of_cities - generate dset case", required=False)
-    parser.add_argument("--max_cost", default=100, help="max cost for each transition - generate dset case", required=False)
-    parser.add_argument("--max_rev", default=300, help="max cost for each transition - generate dset case", required=False)
-    parser.add_argument("--min_rev", default=50, help="max cost for each transition - generate dset case", required=False)
+    parser.add_argument("--max_cost", default=100, help="max cost for each transition - generate dset case",
+                        required=False)
+    parser.add_argument("--max_rev", default=300, help="max cost for each transition - generate dset case",
+                        required=False)
+    parser.add_argument("--min_rev", default=50, help="max cost for each transition - generate dset case",
+                        required=False)
     parser.add_argument("--save_path", default=None, help="path_to_save_dir/filename.npy - generate dset case, if you "
-                                                          "don't wish to save the dataset, don't specify this", required=False)
+                                                          "don't wish to save the dataset, don't specify this",
+                        required=False)
+    # -- hyper parameters
+    parser.add_argument("--p_mutation", default=0.1, help="p in range (0,1) used as bernoulli factor for mutation "
+                                                          "generation", required=False)
+    parser.add_argument("--step_th", default=1e3, help="threshold for max number of generations to consider",
+                        required=False)
+    parser.add_argument("--score_th", default=np.inf, help="threshold for max score", required=False)
+    parser.add_argument("--population_size", default=100, help="population size", required=False)
+    parser.add_argument("--tour_length", default=30, help="number of tour days", required=False)
+
     ret = parser.parse_args()
     return ret
 
