3381ac5ae8378aeda1a638c00b44276686dacae9
itay.rubin <itay,rubin@mail.huji.ac.il>
2021-08-06 18:13:09 +0300

    fixed main csv save

--- analyze_results.py
+++ analyze_results.py
@@ -6,7 +6,7 @@ ANALYZE_PATH = "analyzed_results.csv"
 
 
 def is_interesting(filename):
-    return filename != ANALYZE_PATH and filename[-3:] == 'csv' and int(filename.split('_')[-6]) == 2000
+    return filename != ANALYZE_PATH and filename[-3:] == 'csv'
 
 
 def get_csvs(dir_path):
@@ -20,17 +20,15 @@ def get_csvs(dir_path):
 def parse_csv(csv_path):
     split_path = csv_path.split('_')
     num_cities = int(split_path[2])
-    tour_length = int(split_path[4])
     population_size = int(split_path[-4])
-    elitism_factor = int(split_path[-1])
-    p_mutant = float(split_path[-10])
+    elitism_factor = int(split_path[-1][:-4])
+    p_mutant = float(split_path[4])
 
     df = pd.read_csv(csv_path).rename(columns={"Unnamed: 0": 'iteration'})
     first_max_iteration = df['scores'].argmax()
     max_value = df['scores'].max()
 
     analyzed_df = pd.DataFrame({"num_cities": [num_cities],
-                                "tour_length": [tour_length],
                                 "population_size": [population_size],
                                 "elitism_factor": [elitism_factor],
                                 "p_mutant": [p_mutant],
@@ -44,6 +42,6 @@ if __name__ == '__main__':
     files = get_csvs("results")
     # files.sort(key=lambda file: (file.split('_')[2], file.split('_')[4], file.split('_')[-8], file.split('_')[-2]))
     all_concatenated = pd.concat([parse_csv(f"results/{file}") for file in files])
-    all_concatenated = all_concatenated.sort_values(by=['num_cities', 'tour_length', 'population_size',
+    all_concatenated = all_concatenated.sort_values(by=['num_cities', 'population_size',
                                                         'elitism_factor', 'p_mutant'])
     all_concatenated.to_csv(f"results/{ANALYZE_PATH}", index=False)
