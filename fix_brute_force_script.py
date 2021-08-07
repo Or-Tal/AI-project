8f9d84674a6e36d637279fec599a08b73cdeaf2c
ortal <ortal1602@gmail.com>
2021-08-07 19:18:32 +0300

    started working on GUI

--- fix_brute_force_script.py
+++ fix_brute_force_script.py
@@ -4,6 +4,3 @@ def fix(base_dir):
     for f in os.listdir(base_dir):
         new = f.replace("brute_force", "bruteForce")
         os.system(f"mv {base_dir}/{f} {base_dir}/{new}")
-
-if __name__ == '__main__':
-    fix("noa_kirel/results/small")
