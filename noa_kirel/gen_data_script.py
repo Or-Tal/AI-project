8db0f32cab4fdcc297cfebbc9f4e8b6b457073b1
ortal <ortal1602@gmail.com>
2021-08-08 20:03:50 +0300

    added

--- noa_kirel/gen_data_script.py
+++ noa_kirel/gen_data_script.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     # for i in range(150, 501, 50):
     #     os.system(f"python generate_dataset.py --n {i} --save_path ./datasets/{i}_cities.npy")
     for i in range(3, 16, 3):
-        os.system(f"python generate_dataset.py --n {i} --save_path ./datasets2/{i}_cities.npy")
+        os.system(f"python generate_dataset.py --n {i} --ver 2 --save_path ./datasets2/{i}_cities.npy")
     for i in range(50, 101, 10):
-        os.system(f"python generate_dataset.py --n {i} --save_path ./datasets2/{i}_cities.npy")
+        os.system(f"python generate_dataset.py --n {i} --ver 2 --save_path ./datasets2/{i}_cities.npy")
     for i in range(150, 501, 50):
-        os.system(f"python generate_dataset.py --n {i} --save_path ./datasets2/{i}_cities.npy")
+        os.system(f"python generate_dataset.py --n {i} --ver 2 --save_path ./datasets2/{i}_cities.npy")
