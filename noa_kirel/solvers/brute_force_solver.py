c7b8f7f149f1717474a9e1496e270a03353eaf09
ortal <ortal1602@gmail.com>
2021-08-08 09:28:35 +0300

    stash changes

--- noa_kirel/solvers/brute_force_solver.py
+++ noa_kirel/solvers/brute_force_solver.py
@@ -1,7 +1,7 @@
 import numpy as np
 from noa_kirel.solver import Solver
-from itertools import product
 from noa_kirel.constants import BF_SOL
+from itertools import product
 from time import time
 
 
@@ -16,36 +16,48 @@ class BruteForceSolver(Solver):
         self.rev = revenues
         self.n = int(tour_length)
         self.name = BF_SOL
+        self.best_score = np.NINF
+        self.best_sol = None
+        self.counter = 1
+        self.start_time = 0
 
     def score(self, sol: np.ndarray):
         """
         calculate the score for a given solution
         """
-        acc_score = 0
+        acc_score = list()
         prev = -1
         visited = set()
         for i, x in enumerate(sol):
             r = self.rev[(x, i)] if x not in visited else 0
-            acc_score += r - self.costs[(prev, x)]
+            acc_score.append(r - self.costs[(prev, x)])
             prev = x
             visited.add(x)
-        return acc_score
+        return sum(acc_score)
+
+    def product(self, repeat):
+
+        def single_cell():
+            for i in self.cities:
+                yield i
+
+        generators = [single_cell() for _ in range(repeat)]
+        while True:
+            yield [generators[i].__next__() for i in range(repeat)]
 
     def solve(self):
-        start_time = time()
-        best_score = np.NINF
-        best_sol = None
-        counter = 1
+        self.best_score = np.NINF
+        self.best_sol = None
+        self.counter = 1
+        self.start_time = time()
+
         for sol in product(self.cities, repeat=self.n):
             sol = np.array(sol)
             tmp_score = self.score(sol)
-            if tmp_score > best_score:
-                best_sol = sol
-                best_score = tmp_score
-            yield sol, tmp_score, time() - start_time, best_sol, best_score, counter / self.n
+            if tmp_score > self.best_score:
+                self.best_sol = sol
+                self.best_score = tmp_score
+            yield sol, tmp_score, time() - self.start_time, self.best_sol, self.best_score, self.counter / self.n
 
-        return best_sol, best_score, time() - start_time, best_sol, best_score, 1
-
-
-
+        return self.best_sol, self.best_score, time() - self.start_time, self.best_sol, self.best_score, 1
 
